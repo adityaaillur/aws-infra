731cbe6352e3f1b68ffc0913e3bf0d64fbc2a7aa
adityaaillur <illur.a@northeastern.edu>
2023-04-06 03:45:44 -0400

    update

--- route53.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_route53_record" "a_record" {
-  zone_id = var.zone_id
-  name    = var.a_record_name
-  type    = "A"
-  ttl     = 60
-  records = [aws_instance.ec2_instance.public_ip]
-}
